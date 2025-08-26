2016f03971ac602d9e8b877c26fd0d8d808f9593
Bruno <brunokazuhiro@gmail.com>
2025-08-26 13:20:07 -0300

    feat: add image

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from "react-native"
+import { Image, View } from "react-native"
 import { styles } from "./styles"
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
+      <Image 
+        source={require("../../assets/logo.png")}
+        style={styles.logo}
+      />
     </View>
   )
 }
