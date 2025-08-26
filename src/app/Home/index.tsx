ccc9e5bc3594d786c51ab649e6067daeb1b176cf
Bruno <brunokazuhiro@gmail.com>
2025-08-26 13:22:03 -0300

    feat: use typescript alias

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -5,7 +5,7 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image 
-        source={require("../../assets/logo.png")}
+        source={require("@/assets/logo.png")}
         style={styles.logo}
       />
     </View>
