408b58fdf0e113a49428c53d7142340d8a982e5a
Bruno <brunokazuhiro@gmail.com>
2025-08-26 13:54:08 -0300

    feat: create input component

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -3,6 +3,7 @@ import { Image, View } from "react-native"
 import { Button } from "@/components/Button"
 
 import { styles } from "./styles"
+import { Input } from "@/components/Input"
 
 export function Home() {
   return (
@@ -11,6 +12,7 @@ export function Home() {
         source={require("@/assets/logo.png")}
         style={styles.logo}
       />
+      <Input placeholder="O que você precisa comprar?" />
       <Button title="Entrar" />
  
     </View>
